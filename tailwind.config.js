e1c805d7396625bbc30c80ef9570c123b85559b1
Rifqi222 <mrifki193@gmail.com>
2023-11-11 13:41:10 +0700

    feat: init project with vite, react, tailwind in ts

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  // ...
+  plugins: [require('tailwindcss'), require('autoprefixer')],
+};
+
